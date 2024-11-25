b43a4e88783a83c76db5af825426fdb45dc25650
MarianaLGM <mariana_lobeto@hotmail.com>
2024-11-25 21:40:57 +0100

    últimos cambios

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 
 
-let paisesMundo= document.getElementById("paises-mundo");
-let countriesList= document.getElementById("countries-list");
-let countriesInformation= document.getElementById("countries-information");
-let cerrarVentana= document.getElementById("cerrar-ventana");
-//let abrir= getElementById ("abrir")
-//let cerrar= getElementById ("cerrar")
 
 const getCountries= async()=>{
   try{
@@ -15,56 +9,25 @@ const getCountries= async()=>{
    }
   const data= await response.json();
   // ordenar de mayor a menor
-  ordenarAlf(data);
+  const ordenarPaises = await ordenarAlf(data);
+  return pintarHtml (ordenarPaises);
+
+  /*ordenarAlf(data);
   console.log(data);
+  return data.response;//devolver el resultado 
+  return data //retorno el resultado de los paises ordenados*/
 
-  //devolver el resultado
-  return data.response;
-  //llama aquí a la función de ordenar (si no te lo devuleve ordenado prueba a ponerle un await para que espere a tener la respuesta)
-
-  return data //retorno el resultado de los paises ordenados
     }catch (error){
       console.log ("error al obtener los datos", error);
-  };
-
-  getCountries(countries).then((data)=>
-
-    countries.forEach((elements)=>{ //bucle que recorre cada país con contenedor para mostrar imagen y pais
-      countriesList.innerHTML =` 
-      <img id="abrirVentana" ${elements.flags}>
-      <h2>${elements.name.common}</h2>
-      `;
-      //abrir.addEventListener("click", () => { //ABRIR VENTANA
-       // countriesInformation.classList.add("show");
-     // })
-      paisesMundo.appendChild (countriesList);
-  }) 
-)
-
-
-  //otro bucle para pintar el resto de información  
-  //pte añadir un evento para que cuando hagas clic sobre el contenedor de la bandera abra una ventana con toda la información detallada. 
-  //Botón para cerrar esa ventana.
-   countries.forEach((elements)=>{
-      countriesInformation.innerHTML=`
-      <h3>${elements.capital}</h3>
-      <h3>${elements.population}</h3>
-      <h3>${elements.car.side}</h3>
-      `;
-      //cerrar.addEventListener("click", () => {//CERRAR VENTANA
-       // countriesInformation.classList.remove("show");
-      }) 
-      paisesMundo.appendChild(countriesInformation);
-   // })
-         
+  };       
 }
 
 //llamar a la function
 getCountries();
 
-//función ordenar y letra minúsculas
+/////////ORDENAR ALFABÉTICAMENTE Y PASAR TODO A MINÚSCULAS//////////////
 function ordenarAlf(data) {
-  data.sort((a, b) => {
+  return data.sort((a, b) => {
     const country1 = a.name.common.toLowerCase();
     const country2 = b.name.common.toLowerCase();
 
@@ -77,3 +40,51 @@ function ordenarAlf(data) {
     return 0;
   });
 }
+
+///////////////////BUCLES PARA LOCALIZAR INFO//////////////////////////
+const pintarHtml=(countries)=> {
+  countries.forEach((elements)=>{ 
+    let countriesList= document.getElementById("countries-list"); //contenedor grande
+    let paisesMundo= document.createElement("div"); //contenedor foto y nombre
+    //let abrir= getElementById ("abrir")
+    //let cerrar= getElementById ("cerrar")
+
+    paisesMundo.innerHTML =` 
+    <img id="abrirVentana"  alt="bandera" src="${elements.flags[0]}"/>
+    <h2>${elements.name.common}</h2>
+    `;
+  paisesMundo.addEventListener("click", () => { //ABRIR VENTANA
+   popUp(elements)
+
+   })
+    countriesList.appendChild(paisesMundo);
+  })
+  
+}
+
+function popUp (elements) {
+//crear function para meter todo pop up
+let countriesInformation= document.getElementById("countries-information");//contenedor resto info
+let cerrar= document.createElement("button")
+let contenedor= document.createElement("div")
+contenedor.classList.add ("popUp")
+      contenedor.innerHTML=`
+      <img id="abrirVentana"  alt="bandera" src="${elements.flags[0]}"/>
+      <h3>Pais: ${elements.name.common}</h3>
+      <h3>Capital: ${elements.capital}</h3>
+      <h3>Population: ${elements.population} habitantes</h3>
+      <h3>Car side: ${elements.car.side}</h3>
+      `;
+let popUp= document.getElementsByClassName("popUp");//contenedor resto info
+      cerrar.addEventListener("click", () => {//CERRAR VENTANA
+      
+      countriesInformation.innerHTML=`
+      `;
+      })
+      
+      cerrar.innerHTML="X"
+      contenedor.appendChild(cerrar);
+      countriesInformation.appendChild(contenedor);
+}
+
+    
