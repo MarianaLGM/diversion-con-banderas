1bd9e7ae5ce20e8b828e15de4e21b20edc7b48b7
MarianaLGM <mariana_lobeto@hotmail.com>
2024-11-25 17:14:15 +0100

    cambios

--- js/scritp.js
+++ js/scritp.js
@@ -1,81 +1,79 @@
 
 
-  countries((country) => {
-    const contenedorCountry =document.createElement("li")
-   
-    contenedorCountry.classList.add ("contenedorCountry")
-    
-    contenedorCountry.innerHTML =
-    `
-    <li>${country.name.common}</li>
-    <li>${country.flags}</li>
-     `;
+let paisesMundo= document.getElementById("paises-mundo");
+let countriesList= document.getElementById("countries-list");
+let countriesInformation= document.getElementById("countries-information");
+let cerrarVentana= document.getElementById("cerrar-ventana");
+let abrir= getElementById ("abrir")
+let cerrar= getElementById ("cerrar")
 
-    lista.appendChild (contenedorCountry);
-    countries().then((data)=>contenedorCountry("country".data));
+const getCountries= async(countries)=>{
+  try{
+    const response=await fetch ("https://restcountries.com/v3/all");
+  if(!response.ok){
+     throw new Error ("ha surgido un error", response.status)
+   }
+  const data= await response.json();
+  // ordenar de mayor a menor
+  ordenarAlf(data);
+  console.log(data);
 
-    const contenedorInfoCountry =document.createElement("li")
-    contenedorInfoCountry.classList.add ("contenedorInfoCountry")
-    contenedorInfoCountry.innerHTML =
-    `
-    <li>${country.capital}</li>
-    <li>${country.population}</li>
-    <li>${country.car.side}</li>
-     `;
-     contenedorCountry.appendChild(contenedorInfoCountry);
-     countries().then((data)=>contenedorInfoCountry("country".data));
-   }) 
+  //devolver el resultado
+  return data.response;
+  //llama aquí a la función de ordenar (si no te lo devuleve ordenado prueba a ponerle un await para que espere a tener la respuesta)
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-   const getUsers= async(userId)=>{
-    try{
-      const response=await fetch ("https://jsonplaceholder.typicode.com/todos/");
-     if(!response.ok){
-       throw new Error ("ha surgido un error", response.status)
-     }
-     const data= await response.json();
-     const dataFilterId= data.filter((data)=>data.userId==userId);
-     return dataFilterId
+  return data //retorno el resultado de los paises ordenados
     }catch (error){
       console.log ("error al obtener los datos", error);
-    }
   };
-  const templete=(userId, users)=>{
-    container= document.getElementById(userId)
-    users.forEach(user=>{
-      let templeteUser= `<li>${user.title}</li>`
-      container.innerHTML+=templeteUser
-    })
-   }
-   getUsers(1).then((data)=>templete("userId1", data));
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-   let countriesList= document.getElementById("countries-list"); //CONTENEDOR FOTO+NOMBRE PAIS
-   let countriesInformation= document.getElementById("countries-information")//RESTO INFO PAIS
 
-   const getCountries= async(countries)=>{
-    try{
-      const response=await fetch ("https://restcountries.com/v3/all");
-    if(!response.ok){
-       throw new Error ("ha surgido un error", response.status)
-     }
-    const data= await response.json();
-  return data 
-    }catch (error){
-      console.log ("error al obtener los datos", error);
-    };
+  getCountries(countries).then((data)=>
 
-
-    const templete=(countriesList, countries)=>{
-      container= document.getElementById(countriesList)
-      countries.forEach(country=>{
-        let templeteCountry= `
-        <h2>${country.name.common}</h2>
-        <img ${country.flags}>
-        `;
-        container.innerHTML+=templeteCountry
+    countries.forEach((elements)=>{ //bucle que recorre cada país con contenedor para mostrar imagen y pais
+      countriesList.innerHTML =` 
+      <img id="abrirVentana" ${elements.flags}>
+      <h2>${elements.name.common}</h2>
+      `;
+      abrir.addEventListener("click", () => { //ABRIR VENTANA
+        countriesInformation.classList.add("show");
       })
+      paisesMundo.appendChild (countriesList);
+  }) 
+)
 
-   }
-   getUsers().then((data)=>templete (data))
-  }
+
+  //otro bucle para pintar el resto de información  
+  //pte añadir un evento para que cuando hagas clic sobre el contenedor de la bandera abra una ventana con toda la información detallada. 
+  //Botón para cerrar esa ventana.
+   countries.forEach((elements)=>{
+      countriesInformation.innerHTML=`
+      <h3>${elements.capital}</h3>
+      <h3>${elements.population}</h3>
+      <h3>${elements.car.side}</h3>
+      `;
+      cerrar.addEventListener("click", () => {//CERRAR VENTANA
+        countriesInformation.classList.remove("show");
+      }) 
+      paisesMundo.appendChild(countriesInformation);
+    })
+         
+}
+
+//llamar a la function
+getCountries();
+
+//función ordenar y letra minúsculas
+function ordenarAlf(data) {
+  data.sort((a, b) => {
+    const country1 = a.name.common.toLowerCase();
+    const country2 = b.name.common.toLowerCase();
+
+    if (country1 < country2) {
+      return -1;
+    }
+    if (country1 > country2) {
+      return 1;
+    }
+    return 0;
+  });
+}
